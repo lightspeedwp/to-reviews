2e5e9975d1f53c4e83819cd1ae0d42e0dc9a1530
Warwick Booth <warwick@lsdev.biz>
2019-08-26 15:43:53 +0200

    Added in the new reviews Schema

--- classes/class-to-reviews.php
+++ classes/class-to-reviews.php
@@ -55,38 +55,39 @@ if (!class_exists( 'LSX_TO_Reviews' ) ) {
 			$this->set_vars();
 			$this->lsx_to_search_integration();
 
-			// Make TO last plugin to load
+			// Make TO last plugin to load.
 			add_action( 'activated_plugin', array( $this, 'activated_plugin' ) );
-			
-			add_action('init',array($this,'load_plugin_textdomain'));
 
-			if(false !== $this->post_types){
-				add_filter( 'lsx_to_framework_post_types', array( $this, 'post_types_filter') );
-				add_filter( 'lsx_to_post_types', array( $this, 'post_types_filter') );
-				add_filter( 'lsx_to_post_types_singular', array( $this, 'post_types_singular_filter') );
-				add_filter('lsx_to_settings_path',array( $this, 'plugin_path'),10,2);
+			add_action( 'init', array( $this, 'load_plugin_textdomain' ) );
+
+			if ( false !== $this->post_types ) {
+				add_filter( 'lsx_to_framework_post_types', array( $this, 'post_types_filter' ) );
+				add_filter( 'lsx_to_post_types', array( $this, 'post_types_filter' ) );
+				add_filter( 'lsx_to_post_types_singular', array( $this, 'post_types_singular_filter' ) );
+				add_filter( 'lsx_to_settings_path', array( $this, 'plugin_path' ), 10, 2 );
 			}
-			if(false !== $this->taxonomies){
-				add_filter( 'lsx_to_framework_taxonomies', array( $this, 'taxonomies_filter') );
-				add_filter( 'lsx_to_framework_taxonomies_plural', array( $this, 'taxonomies_plural_filter') );
-			}	
+			if ( false !== $this->taxonomies ) {
+				add_filter( 'lsx_to_framework_taxonomies', array( $this, 'taxonomies_filter' ) );
+				add_filter( 'lsx_to_framework_taxonomies_plural', array( $this, 'taxonomies_plural_filter' ) );
+			}
 
-			require_once(LSX_TO_REVIEWS_PATH . '/classes/class-to-reviews-admin.php');
-			require_once(LSX_TO_REVIEWS_PATH . '/classes/class-to-reviews-frontend.php');
-			require_once(LSX_TO_REVIEWS_PATH . '/classes/class-reviews-schema.php');
-			require_once(LSX_TO_REVIEWS_PATH . '/includes/template-tags.php');
+			require_once LSX_TO_REVIEWS_PATH . '/classes/class-to-reviews-admin.php';
+			require_once LSX_TO_REVIEWS_PATH . '/classes/class-to-reviews-frontend.php';
+			require_once LSX_TO_REVIEWS_PATH . '/includes/template-tags.php';
 
-			// flush_rewrite_rules()
+			// flush_rewrite_rules.
 			register_activation_hook( LSX_TO_REVIEWS_CORE, array( $this, 'register_activation_hook' ) );
 			add_action( 'admin_init', array( $this, 'register_activation_hook_check' ) );
+
+			add_filter( 'wpseo_schema_graph_pieces', array( $this, 'add_graph_pieces' ), 11, 2 );
 		}
 
 		/**
 		 * Include the post type for the search integration
 		 */
 		public function lsx_to_search_integration(){
-			add_filter( 'lsx_to_search_post_types', array( $this, 'post_types_filter') );
-			add_filter( 'lsx_to_search_taxonomies', array( $this, 'taxonomies_filter') );
+			add_filter( 'lsx_to_search_post_types', array( $this, 'post_types_filter' ) );
+			add_filter( 'lsx_to_search_taxonomies', array( $this, 'taxonomies_filter' ) );
 		}
 	
 		/**
@@ -134,10 +135,10 @@ if (!class_exists( 'LSX_TO_Reviews' ) ) {
 		/**
 		 * Adds our post types to an array via a filter
 		 */
-		public function post_types_filter($post_types){
-			if(is_array($post_types) && is_array($this->post_types)){
-				$post_types = array_merge($post_types,$this->post_types);
-			}elseif(is_array($this->post_types)){
+		public function post_types_filter( $post_types ) {
+			if ( is_array( $post_types ) && is_array( $this->post_types ) ) {
+				$post_types = array_merge( $post_types, $this->post_types );
+			} elseif ( is_array( $this->post_types ) ) {
 				$post_types = $this->post_types;
 			}
 			return $post_types;
@@ -146,10 +147,10 @@ if (!class_exists( 'LSX_TO_Reviews' ) ) {
 		/**
 		 * Adds our post types to an array via a filter
 		 */
-		public function post_types_singular_filter($post_types_singular){
-			if(is_array($post_types_singular) && is_array($this->post_types_singular)){
-				$post_types_singular = array_merge($post_types_singular,$this->post_types_singular);
-			}elseif(is_array($this->post_types_singular)){
+		public function post_types_singular_filter( $post_types_singular ) {
+			if ( is_array( $post_types_singular ) && is_array( $this->post_types_singular ) ) {
+				$post_types_singular = array_merge( $post_types_singular, $this->post_types_singular );
+			} elseif ( is_array( $this->post_types_singular ) ) {
 				$post_types_singular = $this->post_types_singular;
 			}
 			return $post_types_singular;
@@ -158,10 +159,10 @@ if (!class_exists( 'LSX_TO_Reviews' ) ) {
 		/**
 		 * Adds our taxonomies to an array via a filter
 		 */
-		public function taxonomies_filter($taxonomies){
-			if(is_array($taxonomies) && is_array($this->taxonomies)){
-				$taxonomies = array_merge($taxonomies,$this->taxonomies);
-			}elseif(is_array($this->taxonomies)){
+		public function taxonomies_filter( $taxonomies ) {
+			if ( is_array( $taxonomies ) && is_array( $this->taxonomies ) ) {
+				$taxonomies = array_merge( $taxonomies, $this->taxonomies );
+			} elseif ( is_array( $this->taxonomies ) ) {
 				$taxonomies = $this->taxonomies;
 			}
 			return $taxonomies;
@@ -170,15 +171,15 @@ if (!class_exists( 'LSX_TO_Reviews' ) ) {
 		/**
 		 * Adds our taxonomies_plural to an array via a filter
 		 */
-		public function taxonomies_plural_filter($taxonomies_plural){
-			if(is_array($taxonomies_plural) && is_array($this->taxonomies_plural)){
-				$taxonomies_plural = array_merge($taxonomies_plural,$this->taxonomies_plural);
-			}elseif(is_array($this->taxonomies_plural)){
+		public function taxonomies_plural_filter( $taxonomies_plural ) {
+			if ( is_array( $taxonomies_plural ) && is_array( $this->taxonomies_plural ) ) {
+				$taxonomies_plural = array_merge( $taxonomies_plural, $this->taxonomies_plural );
+			} elseif ( is_array( $this->taxonomies_plural ) ) {
 				$taxonomies_plural = $this->taxonomies_plural;
 			}
 			return $taxonomies_plural;
 		}
-	
+
 		/**
 		 * Make TO last plugin to load.
 		 */
@@ -196,7 +197,7 @@ if (!class_exists( 'LSX_TO_Reviews' ) ) {
 				}
 			}
 		}
-	
+
 		/**
 		 * On plugin activation
 		 */
@@ -205,7 +206,7 @@ if (!class_exists( 'LSX_TO_Reviews' ) ) {
 				set_transient( '_tour_operators_reviews_flush_rewrite_rules', 1, 30 );
 			}
 		}
-		
+
 		/**
 		 * On plugin activation (check)
 		 */
@@ -217,7 +218,21 @@ if (!class_exists( 'LSX_TO_Reviews' ) ) {
 			delete_transient( '_tour_operators_reviews_flush_rewrite_rules' );
 			flush_rewrite_rules();
 		}
-
+		/**
+		 * Adds Schema pieces to our output.
+		 *
+		 * @param array                 $pieces  Graph pieces to output.
+		 * @param \WPSEO_Schema_Context $context Object with context variables.
+		 *
+		 * @return array $pieces Graph pieces to output.
+		 */
+		public function add_graph_pieces( $pieces, $context ) {
+			if ( class_exists( 'LSX_TO_Schema_Graph_Piece' ) ) {
+				require_once LSX_TO_REVIEWS_PATH . '/classes/class-to-review-schema.php';
+				$pieces[] = new LSX_TO_Schema_Review( $context );
+			}
+			return $pieces;
+		}
 	}
 	new LSX_TO_Reviews();
 }
