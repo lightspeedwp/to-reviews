3d8c3bedc6d30a178a0ba10040b8bafbb067eed6
Warwick Booth <warwick@lsdev.biz>
2019-09-18 17:16:11 +0200

    * Dev - Added in the Review Schema integrated with Yoast WordPress SEO

--- classes/class-to-review-schema.php
+++ classes/class-to-review-schema.php
@@ -28,14 +28,18 @@ class LSX_TO_Schema_Review extends LSX_TO_Schema_Graph_Piece {
 	 * @return array $data Review data.
 	 */
 	public function generate() {
-		$post          = get_post( $this->context->id );
-		$review_author = get_post_meta( $post->ID, 'reviewer_name', true );
-		$review_email  = get_post_meta( $post->ID, 'reviewer_email', true );
-		$rating_value  = get_post_meta( $post->ID, 'rating', true );
-		$description   = wp_strip_all_tags( get_the_content() );
-		$tour_list     = get_post_meta( get_the_ID(), 'tour_to_review', false );
-		$accom_list    = get_post_meta( get_the_ID(), 'accommodation_to_review', false );
-		$comment_count = get_comment_count( $this->context->id );
+		$post                = get_post( $this->context->id );
+		$review_author       = get_post_meta( $post->ID, 'reviewer_name', true );
+		$review_email        = get_post_meta( $post->ID, 'reviewer_email', true );
+		$rating_value        = get_post_meta( $post->ID, 'rating', true );
+		$description         = wp_strip_all_tags( get_the_content() );
+		$tour_list           = get_post_meta( get_the_ID(), 'tour_to_review', false );
+		$accom_list          = get_post_meta( get_the_ID(), 'accommodation_to_review', false );
+		$comment_count       = get_comment_count( $this->context->id );
+		$review_author       = get_post_meta( $post->ID, 'reviewer_name', true );
+		$date_of_visit_start = get_post_meta( $post->ID, 'date_of_visit_start', true );
+		$date_of_visit_end   = get_post_meta( $post->ID, 'date_of_visit_end', true );
+
 		$data          = array(
 			'@type'            => 'Review',
 			'@id'              => $this->context->canonical . '#review',
@@ -58,24 +62,50 @@ class LSX_TO_Schema_Review extends LSX_TO_Schema_Graph_Piece {
 				'bestRating'  => $rating_value,
 			),
 			'reviewBody'       => $description,
-			'itemReviewed'     => \lsx\legacy\Schema_Utils::get_item_reviewed( $tour_list, 'Trip' ),
-			'itemReviewed'     => \lsx\legacy\Schema_Utils::get_item_reviewed( $accom_list, 'Accommodation' ),
+			'itemReviewed'     => \lsx\legacy\Schema_Utils::get_item_reviewed( $tour_list, 'Product' ),
+			'itemReviewed'     => \lsx\legacy\Schema_Utils::get_item_reviewed( $accom_list, 'Product' ),
 		);
 
 		if ( $this->context->site_represents_reference ) {
 			$data['publisher'] = $this->context->site_represents_reference;
 		}
 
+		if ( '' !== $date_of_visit_start && '' !== $date_of_visit_end ) {
+			$data['temporalCoverage'] = $date_of_visit_start . ' - ' . $date_of_visit_end;
+		}
+
 		$data = \lsx\legacy\Schema_Utils::add_image( $data, $this->context );
 		$data = $this->add_taxonomy_terms( $data, 'keywords', 'post_tag' );
 		$data = $this->add_taxonomy_terms( $data, 'reviewSection', 'category' );
+		$data = $this->add_offers( $data );
+		$data = $this->add_destinations( $data );
+		return $data;
+	}
 
-		if ( isset( $_GET['debug'] ) ) {
-			print_r('<pre>');
-			print_r($data);
-			print_r('</pre>');
+	/**
+	 * Adds the Destinations attached to the review
+	 *
+	 * @param array $data Country / State data.
+	 *
+	 * @return array $data Country / State data.
+	 */
+	public function add_destinations( $data ) {
+		$places_array = array();
+		$destinations = get_post_meta( $this->context->id, 'destination_to_' . $this->post_type, false );
+		if ( ! empty( $destinations ) ) {
+			foreach ( $destinations as $destination_id ) {
+				if ( '' !== $destination_id ) {
+					if ( 0 === wp_get_post_parent_id( $destination_id ) || false === wp_get_post_parent_id( $destination_id ) ) {
+						$places_array = \lsx\legacy\Schema_Utils::add_place( $places_array, 'Country', $destination_id, $this->context );
+					} else {
+						$places_array = \lsx\legacy\Schema_Utils::add_place( $places_array, 'State', $destination_id, $this->context );
+					}
+				}
+			}
 		}
-
+		if ( ! empty( $places_array ) ) {
+			$data['spatialCoverage'] = $places_array;
+		}
 		return $data;
 	}
 }
